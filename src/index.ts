81fa3151ceba00a7bb439e6e9420b0f5b9f59584
Jack Holgado <jack@Jacks-MacBook-Pro.local>
2022-01-17 21:42:24 -0800

    init

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+// Stripe key env variables
+import { config } from "dotenv"
+import express, { Request, Response, NextFunction } from "express";
+
+if (process.env.NODE_ENV !== 'production') {
+  // use whatever env variables in docker container or server
+  config();
+  console.log('loaded prod env variables');
+}
+
+//init stripe
+import Stripe from 'stripe'
+export const stripe = new Stripe(process.env.STRIPE_SECRET, {
+  apiVersion: '2020-08-27',
+});
+
+//init express; braces for app because it was exported
+import { app } from './api';
+const port = process.env.PORT || 3333;
+//backtick for string templatings
+app.listen(port, () => console.log(`Api available on port ${port}`));
